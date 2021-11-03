592b1bad830c934425d852729d7ba5a219d7e2e2
deluvsushi <77536370+deluvsushi@users.noreply.github.com>
2021-11-03 16:26:01 +0600

    Create menu_configs.py

---
+++ configs/menu_configs.py
@@ -0,0 +1,72 @@
+main_menu = [
+    ["Info >> ", "Script By DeLuvSushi", "Amino-Utilz"],
+    ["Telegram >> ", "https://t.me/aminobotsdelav"],
+    ["1", "Spam-Utilz"],
+    ["2", "Active-Utilz"],
+    ["3", "Other-Utilz"],
+    ["4", "Profile-Utilz"]
+]
+
+
+spam_utilz_menu = [
+    ["Info >> ", "Script By DeLuvSushi", "Amino-Utilz"],
+    ["Telegram >> ", "https://t.me/aminobotsdelav"],
+    ["1", "Spam Comments"],
+    ["2", "Spam Message"],
+    ["3", "Spam With Join And Leave"],
+    ["4", "Spam Reports"]
+]
+
+
+spam_comments_menu = [
+    ["Info >> ", "Script By DeLuvSushi", "Amino-Utilz"],
+    ["Telegram >> ", "https://t.me/aminobotsdelav"],
+    ["1", "Spam Comments to Wiki"],
+    ["2", "Spam Comments to Blog"],
+    ["3", "Spam Comments to User"]
+]
+
+
+spam_reports_menu = [
+    ["Info >> ", "Script By DeLuvSushi", "Amino-Utilz"],
+    ["Telegram >> ", "https://t.me/aminobotsdelav"],
+    ["1", "Spam Reports to Chat"],
+    ["2", "Spam Reports to User"],
+    ["3", "Spam Reports to Wiki"],
+    ["4", "Spam Reports to Blog"]
+]
+
+
+active_utilz_menu = [
+    ["Info >> ", "Script By DeLuvSushi", "Amino-Utilz"],
+    ["Telegram >> ", "https://t.me/aminobotsdelav"],
+    ["1", "Follow Bot"],
+    ["2", "Increase Time(Slow)"],
+    ["3", "Like Bot"],
+    ["4", "Watch Ad"]
+]
+
+other_utilz_menu = [
+    ["Info >> ", "Script By DeLuvSushi", "Amino-Utilz"],
+    ["Telegram >> ", "https://t.me/aminobotsdelav"],
+    ["1", "Join Active Chats"],
+    ["2", "Join Recommended Communities"],
+    ["3", "Leave From All Joined Chats"],
+    ["4", "Leave From All Joined Communities"],
+    ["5", "Get Blocker, Blocked Users List"],
+    ["6", "Get Global User Information"],
+    ["7", "Delete Chat(Need Host)"],
+    ["8", "Get Community Information"],
+    ["9", "Get Global User Info"],
+    ["10", "Get Chat Info"],
+    ["11", "Get Account Info"]
+]
+
+
+profile_utilz_menu = [
+    ["Info >> ", "Script By DeLuvSushi", "Amino-Utilz"],
+    ["Telegram >> ", "https://t.me/aminobotsdelav"],
+    ["1", "AntiBan"],
+    ["2", "Edit Nickname"],
+    ["3", "Edit Content"]
+]
